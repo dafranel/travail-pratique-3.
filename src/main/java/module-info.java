2bdba5a055574b71e9b598838269cca2cce9c71c
dafranel <chendjoutalladanyfranknelson@gmail.com>
2026-04-29 11:13:34 -0400

    Initialisation du projet en programmation graphique TP3

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.travailpratique3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media;
+
+
+    opens com.example.travailpratique3 to javafx.fxml;
+    exports com.example.travailpratique3;
+}
